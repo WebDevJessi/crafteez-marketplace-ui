d1c69378cb1523eca45aaf2609d8ee4a52b55e9a
WebDevJess <gutierrez.jessica792@gmail.com>
2026-07-14 16:19:27 -0700

    more changes

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,11 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { IGX_CAROUSEL_DIRECTIVES, IgxCarouselModule, IgxIconComponent, IgxIconModule } from 'igniteui-angular';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [IgxCarouselModule, IGX_CAROUSEL_DIRECTIVES, NgFor, IgxIconComponent, IgxIconModule],
 templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -11,4 +13,9 @@ export class HomepageComponent {
   appTitle = 'Crafteez';
   subTitle = 'Crafting all kinds of things';
   currentBackground = "url('assets/background.jpg')";
+  public slides = [
+    { src: '/assets/ceramic.jpg' },
+    { src: '/assets/sticker.jpg' },
+    { src: '/assets/star.svg' },
+  ]
 }

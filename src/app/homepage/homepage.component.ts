de36c525c8fdf3ce10c3f6417938840b702c1ddf
WebDevJess <gutierrez.jessica792@gmail.com>
2026-07-15 11:44:36 -0700

    add more fixes to carousel

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,11 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { IGX_CAROUSEL_DIRECTIVES, IgxCarouselModule, IgxIconComponent, IgxIconModule } from 'igniteui-angular';
+import { IGX_CAROUSEL_DIRECTIVES, IgxCarouselModule, IgxIconComponent, IgxIconModule, IgxCardActionsComponent } from 'igniteui-angular';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [IgxCarouselModule, IGX_CAROUSEL_DIRECTIVES, NgFor, IgxIconComponent, IgxIconModule],
+  imports: [IgxCarouselModule, IGX_CAROUSEL_DIRECTIVES, NgFor, IgxIconComponent, IgxIconModule, IgxCardActionsComponent],
 templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
@@ -14,8 +14,8 @@ export class HomepageComponent {
   subTitle = 'Crafting all kinds of things';
   currentBackground = "url('assets/background.jpg')";
   public slides = [
-    { src: '/assets/ceramic.jpg' },
+    { src: '/assets/pins.jpg' },
     { src: '/assets/sticker.jpg' },
-    { src: '/assets/star.svg' },
+    { src: '/assets/postcards.jpg' },
   ]
 }
